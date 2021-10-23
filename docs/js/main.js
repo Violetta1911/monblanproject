01e0006af0ca1650efec55a28ce0e36f4e9ee455
Violetta1911 <sva198366@gmail.com>
2021-10-24 01:22:27 +0300

    without adaptive

---
+++ docs/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+	$('.datepicker').datepicker({
+		buttonImage: '/images/calendar.png',
+		constrainInput: false,
+		closeText: 'Close',
+		showButtonPanel: true,
+		showButtonText: 'from',
+		buttonImageOnly: true,
+		showOn: 'button',
+	});
+});
